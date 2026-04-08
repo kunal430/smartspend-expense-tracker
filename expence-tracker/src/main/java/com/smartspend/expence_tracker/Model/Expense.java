9726b146c7b779fb14489c7d4671c53c5dc8ed2b
kunal430 <kunalofficial5188@gmail.com>
2026-04-08 08:29:31 +0530

    create model

--- expence-tracker/src/main/java/com/smartspend/expence_tracker/Model/Expense.java
+++ expence-tracker/src/main/java/com/smartspend/expence_tracker/Model/Expense.java
@@ -1,4 +1,42 @@
 package com.smartspend.expence_tracker.Model;
 
-public class Expense gi{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Expense {
+    @Id
+    @GeneratedValue
+    private Long Id;
+
+
+    private String title ;
+    private Double amount ;
+
+    public  Expense () {}
+    public Expense(String title , Double amount ){
+        this.amount = amount ;
+        this.title = title ;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
