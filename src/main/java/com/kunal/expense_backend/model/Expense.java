fd8d2a647544960cd1fae09d7a76113c8654b1b7
kunal430 <kunalofficial5188@gmail.com>
2026-05-07 01:05:15 +0530

    feat: reset project structure to show files at root

---
+++ src/main/java/com/kunal/expense_backend/model/Expense.java
@@ -0,0 +1,4 @@
+package com.kunal.expense_backend.model;
+
+public class Expense {
+}
---
+++ src/main/java/com/kunal/expense_backend/service/ExpenseService.java
@@ -0,0 +1,7 @@
+package com.kunal.expense_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpenseService {
+}
