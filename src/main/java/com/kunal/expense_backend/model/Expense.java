bebbd6a80119cb2132278e2954617612fca1633b
kunal430 <kunalofficial5188@gmail.com>
2026-05-07 22:23:35 +0530

    add Expence model

--- src/main/java/com/kunal/expense_backend/model/Expense.java
+++ src/main/java/com/kunal/expense_backend/model/Expense.java
@@ -1,4 +1,44 @@
 package com.kunal.expense_backend.model;
 
 public class Expense {
+    private String tittle ;
+
+    private double amount ;
+
+    private String description ;
+
+    public Expense() {
+
+    }
+
+    public Expense(String tittle , double amount, String description) {
+        this.tittle= tittle ;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+
+    public double getAmount(){
+        return amount ;
+    }
+
+    public void setAmount(double amount){
+        this.amount = amount ;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
