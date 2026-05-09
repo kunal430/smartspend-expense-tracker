b48594915ff027963c6d8e98d230040f047d9e18
kunal430 <kunalofficial5188@gmail.com>
2026-05-09 23:48:46 +0530

    minor changes

--- src/main/java/com/kunal/expense_backend/controller/ExpenseController.java
+++ src/main/java/com/kunal/expense_backend/controller/ExpenseController.java
@@ -1,7 +1,18 @@
 package com.kunal.expense_backend.controller;
 
+import com.kunal.expense_backend.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/expense")
 public class ExpenseController {
+    private final ExpenseService expenseService ;
+
+    @Autowired
+
+    public ExpenseController (ExpenseService expenseService){
+        this.expenseService=expenseService ;
+    }
 }
--- src/main/java/com/kunal/expense_backend/service/ExpenseService.java
+++ src/main/java/com/kunal/expense_backend/service/ExpenseService.java
@@ -1,4 +1,5 @@
 package com.kunal.expense_backend.service;
+import com.kunal.expense_backend.model.Expense;
 import com.kunal.expense_backend.repository.ExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ public class ExpenseService {
     public ExpenseService (ExpenseRepository expenseRepository) {
         this.expenseRepository = expenseRepository ;
     }
-
-
+    public Expense crteateExpence(Expense expense){
+        return expenseRepository.save(expense);
+    }
 }
